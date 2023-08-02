15ec00e995214de1d3dbea6f96950e4e5e4eabd6
Carlisle Kotzee <cjkotzee@gmail.com>
2023-08-02 13:06:41 +0200

    [#3}

--- tut_0/greeter.java
+++ tut_0/greeter.java
@@ -1,6 +1,12 @@
-public class Main {
+public class Greeter {
     
     public static void main(String[] args) {
-        
+        String name = args[0];
+        greeter(name);
+    }
+
+    public static void greeter(String name)
+    {
+        System.out.println("Hello " + name + ", I have come here to provide you with a quests");
     }
 }
